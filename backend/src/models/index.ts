9b1eff9d73eaf8df69a597d2864f5a534197a175
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-01-25 18:34:11 +0100

    Create index.ts

---
+++ backend/src/models/index.ts
@@ -0,0 +1,5 @@
+export { UserModel } from './User';
+// Additional models will be exported here as they are created
+// export { GameModel } from './Game';
+// export { TeamModel } from './Team';
+// export { BettingSlipModel } from './BettingSlip';
